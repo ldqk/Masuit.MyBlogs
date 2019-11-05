5e1de7af2dcee7fa60ccbaf33e900b92d18031d9
懒得勤快 <20254980+ldqk@users.noreply.github.com>
2019-11-05 14:56:21 +0800

    Update NonWwwRule.cs

--- src/Masuit.MyBlogs.Core/Extensions/NonWwwRule.cs
+++ src/Masuit.MyBlogs.Core/Extensions/NonWwwRule.cs
@@ -17,7 +17,7 @@ namespace Masuit.MyBlogs.Core.Extensions
 
             if (req.Scheme.Equals("http") || currentHost.Host.Contains("www."))
             {
-                context.HttpContext.Response.Redirect("https://" + currentHost.Host.Replace("www.", string.Empty) + req.PathBase + req.Path + req.QueryString);
+                context.HttpContext.Response.Redirect("https://" + currentHost.Value.Replace("www.", string.Empty) + req.PathBase + req.Path + req.QueryString);
                 context.Result = RuleResult.EndResponse;
             }
         }
