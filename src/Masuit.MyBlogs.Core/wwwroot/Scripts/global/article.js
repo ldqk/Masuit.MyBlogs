1815bfb1fbfeb9e855960ddbc97872b2b43eb9aa
懒得勤快 <admin@masuit.com>
2020-03-08 21:43:18 +0800

    兼容性

--- src/Masuit.MyBlogs.Core/wwwroot/Scripts/global/article.js
+++ src/Masuit.MyBlogs.Core/wwwroot/Scripts/global/article.js
@@ -310,26 +310,14 @@ function commentVoteBind() {
                     $(this).children("span.count").text(parseInt($(this).children("span.count").text())+1);
 					$(this).addClass("disabled");
 					this.disabled = true;
-					window.notie.alert({
-						type: 1,
-						text: data.Message,
-						time: 4
-					});
+					window.notie.alert({ type: 1, text: data.Message, time: 4 });
 				} else {
-					window.notie.alert({
-						type: 3,
-						text: data.Message,
-						time: 4
-					});
+					window.notie.alert({ type: 3, text: data.Message, time: 4 });
 				}
 			}
 		});
 	}, function() {
-        window.notie.alert({
-			type: 3,
-			text: "请求失败，请稍候再试！",
-			time: 4
-		});
+        window.notie.alert({ type: 3, text: "请求失败，请稍候再试！", time: 4 });
     });
 }
 
@@ -338,7 +326,7 @@ function bindVote() {
         window.post("/post/voteup", { id: $("#postId").val() }, function(data) {
 			if (data) {
 				if (data.Success) {
-					$(this).children()[1].innerText = parseInt($(this).children()[1].innerText) + 1;
+					$("#voteup span").text(parseInt($("#voteup span").text()) + 1);
 					$(this).addClass("disabled");
 					this.disabled = true;
 					window.notie.alert({
@@ -366,7 +354,7 @@ function bindVote() {
         window.post("/post/votedown", { id: $("#postId").val() }, function(data) {
 			if (data) {
 				if (data.Success) {
-					$(this).children()[1].innerText = parseInt($(this).children()[1].innerText) + 1;
+					$("#votedown span").text(parseInt($("#votedown span").text()) + 1);
 					$(this).addClass("disabled");
 					this.disabled = true;
 					window.notie.alert({
