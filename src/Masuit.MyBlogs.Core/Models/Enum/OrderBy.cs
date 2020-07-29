4f755d646a3bb28571dd3752086a88ed14803c32
懒得勤快 <admin@masuit.com>
2020-07-29 15:51:27 +0800

    一些前端优化

--- src/Masuit.MyBlogs.Core/Models/Enum/OrderBy.cs
+++ src/Masuit.MyBlogs.Core/Models/Enum/OrderBy.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Masuit.MyBlogs.Core.Models.Enum
 {
@@ -10,36 +11,42 @@ namespace Masuit.MyBlogs.Core.Models.Enum
         /// <summary>
         /// 按发表时间
         /// </summary>
+        [Description("最后发表时间")]
         [Display(Name = nameof(PostDate))]
         PostDate,
 
         /// <summary>
         /// 按修改时间
         /// </summary>
+        [Description("最后修改时间")]
         [Display(Name = nameof(ModifyDate))]
         ModifyDate,
 
         /// <summary>
         /// 按访问次数
         /// </summary>
+        [Description("访问量最多")]
         [Display(Name = nameof(TotalViewCount))]
         TotalViewCount,
 
         /// <summary>
         /// 按评论数
         /// </summary>
+        [Description("评论最多")]
         [Display(Name = nameof(CommentCount))]
         CommentCount,
 
         /// <summary>
         /// 按投票数
         /// </summary>
+        [Description("支持最多")]
         [Display(Name = nameof(VoteUpCount))]
         VoteUpCount,
 
         /// <summary>
         /// 每日平均访问量
         /// </summary>
+        [Description("最热门")]
         [Display(Name = nameof(AverageViewCount))]
         AverageViewCount,
     }
