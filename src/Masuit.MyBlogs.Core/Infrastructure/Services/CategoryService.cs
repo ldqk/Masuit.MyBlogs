ffa923bdb571d9f5d6e32e5e3ee069d0b1d82846
懒得勤快 <admin@masuit.com>
2020-08-13 11:40:26 +0800

    删除分类的bug

--- src/Masuit.MyBlogs.Core/Infrastructure/Services/CategoryService.cs
+++ src/Masuit.MyBlogs.Core/Infrastructure/Services/CategoryService.cs
@@ -2,12 +2,16 @@
 using Masuit.MyBlogs.Core.Infrastructure.Repository.Interface;
 using Masuit.MyBlogs.Core.Infrastructure.Services.Interface;
 using Masuit.MyBlogs.Core.Models.Entity;
-using Masuit.Tools;
+using System.Linq;
 
 namespace Masuit.MyBlogs.Core.Infrastructure.Services
 {
     public partial class CategoryService : BaseService<Category>, ICategoryService
     {
+        public CategoryService(IBaseRepository<Category> repository, ISearchEngine<DataContext> searchEngine, ILuceneIndexSearcher searcher) : base(repository, searchEngine, searcher)
+        {
+        }
+
         /// <summary>
         /// 删除分类，并将该分类下的文章移动到新分类下
         /// </summary>
@@ -16,24 +20,27 @@ namespace Masuit.MyBlogs.Core.Infrastructure.Services
         /// <returns></returns>
         public bool Delete(int id, int mid)
         {
-            Category category = GetById(id);
-            Category moveCat = GetById(mid);
-            category.Post.ForEach(p =>
+            var category = GetById(id);
+            var moveCat = GetById(mid);
+            for (var j = 0; j < category.Post.Count; j++)
             {
+                var p = category.Post.ElementAt(j);
                 moveCat.Post.Add(p);
-                p.PostHistoryVersion.ForEach(v => moveCat.PostHistoryVersion.Add(v));
-            });
-            category.PostHistoryVersion.ForEach(p =>
+                for (var i = 0; i < p.PostHistoryVersion.Count; i++)
+                {
+                    moveCat.PostHistoryVersion.Add(p.PostHistoryVersion.ElementAt(i));
+                }
+            }
+
+            for (var i = 0; i < category.PostHistoryVersion.Count; i++)
             {
+                var p = category.PostHistoryVersion.ElementAt(i);
+                p.CategoryId = moveCat.Id;
                 moveCat.PostHistoryVersion.Add(p);
-            });
+            }
 
             bool b = DeleteByIdSaved(id);
             return b;
-        }
-
-        public CategoryService(IBaseRepository<Category> repository, ISearchEngine<DataContext> searchEngine, ILuceneIndexSearcher searcher) : base(repository, searchEngine, searcher)
-        {
         }
     }
 }
