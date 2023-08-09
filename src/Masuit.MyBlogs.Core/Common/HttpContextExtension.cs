e5e13e3f0f061db71e6fa25fe9fe064e4a16e888
懒得勤快 <admin@masuit.com>
2023-08-09 22:47:27 +0800

    升级包

--- src/Masuit.MyBlogs.Core/Common/HttpContextExtension.cs
+++ src/Masuit.MyBlogs.Core/Common/HttpContextExtension.cs
@@ -6,54 +6,57 @@ namespace Masuit.MyBlogs.Core.Common;
 
 public static class HttpContextExtension
 {
-    /// <summary>
-    /// 地理位置信息
-    /// </summary>
-    /// <param name="request"></param>
-    /// <returns></returns>
-    public static IPLocation Location(this HttpRequest request)
-    {
-        return (IPLocation)request.HttpContext.Items.GetOrAdd("ip.location", request.HttpContext.Connection.RemoteIpAddress.GetIPLocation);
-    }
+	/// <summary>
+	/// 地理位置信息
+	/// </summary>
+	/// <param name="request"></param>
+	/// <returns></returns>
+	public static IPLocation Location(this HttpRequest request)
+	{
+		return (IPLocation)request.HttpContext.Items.GetOrAdd("ip.location", request.HttpContext.Connection.RemoteIpAddress.GetIPLocation);
+	}
 
-    public static int[] GetHideCategories(this HttpRequest request)
-    {
-        return request.Cookies[SessionKey.HideCategories]?.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => s.ToInt32()).ToArray() ?? request.Query[SessionKey.SafeMode].ToString().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => s.ToInt32()).ToArray();
-    }
+	public static int[] GetHideCategories(this HttpRequest request)
+	{
+		return request.Cookies[SessionKey.HideCategories]?.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => s.ToInt32()).ToArray() ?? request.Query[SessionKey.SafeMode].ToString().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => s.ToInt32()).ToArray();
+	}
 
-    /// <summary>
-    /// 是否是搜索引擎访问
-    /// </summary>
-    /// <param name="req"></param>
-    /// <returns></returns>
-    public static bool IsRobot(this HttpRequest req)
-    {
-        if (UserAgent.Parse(req.Headers[HeaderNames.UserAgent].ToString()).IsRobot || req.Location().Contains("Spider", "蜘蛛"))
-        {
-            var nslookup = new LookupClient();
-            var fallbackPolicy = Policy<bool>.Handle<Exception>().FallbackAsync(false);
-            var retryPolicy = Policy<bool>.Handle<Exception>().RetryAsync(3);
-            return Policy.WrapAsync(fallbackPolicy, retryPolicy).ExecuteAsync(async () =>
-            {
-                using var cts = new CancellationTokenSource(1000);
-                var query = await nslookup.QueryReverseAsync(req.HttpContext.Connection.RemoteIpAddress, cts.Token);
-                return query.Answers.Any(r => r.ToString().Trim('.').EndsWith(new[]
-                {
-                    "baidu.com",
-                    "google.com",
-                    "googlebot.com",
-                    "googleusercontent.com",
-                    "bing.com",
-                    "search.msn.com",
-                    "sogou.com",
-                    "soso.com",
-                    "yandex.com",
-                    "apple.com",
-                    "sm.cn"
-                }));
-            }).Result;
-        }
+	private static readonly string[] Spiders = {
+		"baidu.com",
+		"google.com",
+		"googlebot.com",
+		"googleusercontent.com",
+		"bing.com",
+		"search.msn.com",
+		"sogou.com",
+		"soso.com",
+		"yandex.com",
+		"apple.com",
+		"sm.cn",
+		"telegram.org",
+		"twttr.com"
+	};
 
-        return false;
-    }
+	/// <summary>
+	/// 是否是搜索引擎访问
+	/// </summary>
+	/// <param name="req"></param>
+	/// <returns></returns>
+	public static bool IsRobot(this HttpRequest req)
+	{
+		if (UserAgent.Parse(req.Headers[HeaderNames.UserAgent].ToString()).IsRobot || req.Location().Contains("Spider", "蜘蛛"))
+		{
+			var nslookup = new LookupClient();
+			var fallbackPolicy = Policy<bool>.Handle<Exception>().FallbackAsync(false);
+			var retryPolicy = Policy<bool>.Handle<Exception>().RetryAsync(3);
+			return Policy.WrapAsync(fallbackPolicy, retryPolicy).ExecuteAsync(async () =>
+			{
+				using var cts = new CancellationTokenSource(1000);
+				var query = await nslookup.QueryReverseAsync(req.HttpContext.Connection.RemoteIpAddress, cts.Token);
+				return query.Answers.Any(r => r.ToString().Trim('.').EndsWith(Spiders));
+			}).Result;
+		}
+
+		return false;
+	}
 }
